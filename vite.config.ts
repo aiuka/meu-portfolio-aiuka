c1e9e46d8d639d5dd23d4d4f0f5fa30de1f491fe
Aiuka Machado <40308155+aiuka@users.noreply.github.com>
2025-05-27 22:56:59 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
-  base: '/meu-portfolio-aiuka/',
+  plugins: [react()],
   server: {
     host: '::',
-    port: 8080,
+    port: 8080
   },
-  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+      '@': '/src'
+    }
+  }
 });
