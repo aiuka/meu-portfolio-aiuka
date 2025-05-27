f1ab37c197f5875bc442ae65db7a6e8a7669299f
Aiuka Machado <40308155+aiuka@users.noreply.github.com>
2025-05-27 21:22:07 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import componentTagger from 'vite-plugin-component-tagger'; // Adjust if this is a custom plugin or different package
+
 export default defineConfig(({ mode }) => ({
-  base: '/meu-portfolio-aiuka/', // Adicione esta linha
+  base: '/meu-portfolio-aiuka/',
   server: {
-    host: "::",
+    host: '::',
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 }));
